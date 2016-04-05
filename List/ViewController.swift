cdc65127264f39694e808cd60c96495dcbc0ac97
paulusherdiyatma <paulusherdiyatma@gmail.com>
2016-04-05 22:25:32 +0800

    Initial Commit

---
+++ List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by paulus herdiyatma on 4/5/16.
+//  Copyright © 2016 paulus herdiyatma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
